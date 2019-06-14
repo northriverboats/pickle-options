19a3a556dcd3a6c1c35c2e8cb393bda6f4442743
Fred Warren <fred.warren@gmail.com>
2019-06-14 08:00:17 -0700

    Basic Main Window runs

---
+++ MainWindow.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(601, 193)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.horizontalLayout_6 = QtGui.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
+        self.lblFile = QtGui.QLabel(self.centralwidget)
+        self.lblFile.setText(_fromUtf8(""))
+        self.lblFile.setObjectName(_fromUtf8("lblFile"))
+        self.horizontalLayout_6.addWidget(self.lblFile)
+        self.gridLayout.addLayout(self.horizontalLayout_6, 4, 0, 1, 1)
+        self.horizontalLayout_8 = QtGui.QHBoxLayout()
+        self.horizontalLayout_8.setObjectName(_fromUtf8("horizontalLayout_8"))
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_8.addItem(spacerItem)
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout_8.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.horizontalLayout_8.addWidget(self.pushButton_3)
+        self.gridLayout.addLayout(self.horizontalLayout_8, 6, 0, 1, 1)
+        self.horizontalLayout_5 = QtGui.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(_fromUtf8("horizontalLayout_5"))
+        self.progressBar = QtGui.QProgressBar(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.progressBar.sizePolicy().hasHeightForWidth())
+        self.progressBar.setSizePolicy(sizePolicy)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName(_fromUtf8("progressBar"))
+        self.horizontalLayout_5.addWidget(self.progressBar)
+        self.gridLayout.addLayout(self.horizontalLayout_5, 3, 0, 1, 1)
+        self.horizontalLayout_7 = QtGui.QHBoxLayout()
+        self.horizontalLayout_7.setObjectName(_fromUtf8("horizontalLayout_7"))
+        spacerItem1 = QtGui.QSpacerItem(20, 20, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.horizontalLayout_7.addItem(spacerItem1)
+        self.gridLayout.addLayout(self.horizontalLayout_7, 5, 0, 1, 1)
+        self.horizontalLayout_4 = QtGui.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(_fromUtf8("horizontalLayout_4"))
+        self.lePath = QtGui.QLineEdit(self.centralwidget)
+        self.lePath.setObjectName(_fromUtf8("lePath"))
+        self.horizontalLayout_4.addWidget(self.lePath)
+        self.btnBrowse = QtGui.QPushButton(self.centralwidget)
+        self.btnBrowse.setObjectName(_fromUtf8("btnBrowse"))
+        self.horizontalLayout_4.addWidget(self.btnBrowse)
+        self.gridLayout.addLayout(self.horizontalLayout_4, 2, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 601, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName(_fromUtf8("menuFile"))
+        self.menuHelp = QtGui.QMenu(self.menubar)
+        self.menuHelp.setObjectName(_fromUtf8("menuHelp"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionAbout = QtGui.QAction(MainWindow)
+        self.actionAbout.setObjectName(_fromUtf8("actionAbout"))
+        self.actionRun = QtGui.QAction(MainWindow)
+        self.actionRun.setObjectName(_fromUtf8("actionRun"))
+        self.actionCancel = QtGui.QAction(MainWindow)
+        self.actionCancel.setObjectName(_fromUtf8("actionCancel"))
+        self.actionClose = QtGui.QAction(MainWindow)
+        self.actionClose.setObjectName(_fromUtf8("actionClose"))
+        self.menuFile.addAction(self.actionRun)
+        self.menuFile.addAction(self.actionCancel)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionClose)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Options Folder Pickler", None))
+        self.pushButton_2.setText(_translate("MainWindow", "&Cancel", None))
+        self.pushButton_3.setText(_translate("MainWindow", "&Run", None))
+        self.btnBrowse.setText(_translate("MainWindow", "&Browse", None))
+        self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help", None))
+        self.actionAbout.setText(_translate("MainWindow", "About", None))
+        self.actionRun.setText(_translate("MainWindow", "Run", None))
+        self.actionCancel.setText(_translate("MainWindow", "Cancel", None))
+        self.actionClose.setText(_translate("MainWindow", "Close", None))
+
--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from PyQt4 import QtCore
 import sys # We need sys so that we can pass argv to QApplication
 import os
 import re
-import MainDialog
+import MainWindow  # This file holds our MainWindow and all design related things
 
-class MainDialogWindow(QtGui.QDialog, MainDialog.Ui_Dialog):
+class MainAppWindow(QtGui.QMainWindow, MainWindow.Ui_MainWindow):
+
     def __init__(self):
-        QtGui.QDialog.__init__(self)
+        super(self.__class__, self).__init__()
         self.setupUi(self)
-        # self.buttonBox.rejected.connect(self.close)
-        # self.textResults.setText( QtCore.QString(text) )
-        # self.textResults.setReadOnly(True)
 
 def main():
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
-    form = MainDialogWindow()                 # We set the form to be our ExampleApp (design)
+    form = MainAppWindow()                 # We set the form to be our Main App Wehdiw (design)
     form.show()                         # Show the form
     app.exec_()                         # and execute the app
 
 
 if __name__ == '__main__':              # if we're running file directly and not importing it
     main()                              # run the main function
+                       # run the main function
